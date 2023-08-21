7f55950d7f70b04ec6d441810f17f9a437e05e6c
9Satis <142721604+9Satis@users.noreply.github.com>
2023-08-21 13:57:07 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionopertion.js
@@ -0,0 +1,13 @@
+let score = "33abc"
+console.log(typeof(score))
+let valuenumber = Number(score)
+console.log(valuenumber)
+
+// "33"  "33abc =nan"
+//true   = 1
+//false = 0
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof (stringnumber));
